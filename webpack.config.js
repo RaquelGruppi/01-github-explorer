fe8cec8fb2b17a74807ef29cb1c4fd7c18bd9c26
Raquel Gruppi <raquel.gruppi@gmail.com>
2021-03-16 14:20:13 +1100

    Add html webpack plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
-const { dirname } = require('path');
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
@@ -11,6 +11,12 @@ module.exports = {
   resolve: {
     extensions: ['.js','.jsx'],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'public', 'index.html'),
+      inject: "body"
+    })
+  ],
   module: {
     rules: [
       {
